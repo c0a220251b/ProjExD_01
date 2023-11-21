df0270fd352bb3b9e678ceccdf9c97b017d897d6
Rentaro <c0a220251b@edu.teu.ac.jp>
2023-11-21 16:37:48 +0900

    課題1

--- pygame_image.py
+++ pygame_image.py
@@ -10,8 +10,9 @@ def main():
     kk_img = pg.image.load("ex01/fig/3.png") #練習1
     tmr = 0
     kk_hanten = pg.transform.flip(kk_img, True, False) #練習2 画像の反転
-    kk_kaiten = pg.transform.rotozoom(kk_hanten, 10, 1.0) 
-    kk_imgs = [kk_hanten, kk_kaiten] #練習3　画像の回転
+    #練習3　画像の回転
+    kk_imgs = [pg.transform.rotozoom(kk_hanten, i, 1.0) for i in range(11)]
+    c = [0,1,2,3,4,5,6,7,8,9,10,9,8,7,6,5,4,3,2,1,0]
     
     while True:
         for event in pg.event.get():
@@ -19,7 +20,7 @@ def main():
         x = tmr%1600  #練習6　動く背景画像
         screen.blit(bg_img, [-x, 0]) #練習4
         screen.blit(bg_img, [1600-x, 0]) 
-        screen.blit(kk_imgs[tmr%2], [300, 200]) #練習5
+        screen.blit(kk_imgs[c[tmr%20]], [300, 200]) #練習5
         pg.display.update()
         tmr += 1        
         clock.tick(100)
